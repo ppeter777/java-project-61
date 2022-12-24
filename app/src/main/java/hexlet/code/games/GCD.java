8fa176c5c2be9c01e6f87c7dadc5f788b557b4d4
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 02:16:59 +0300

    code simplifications in GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -12,14 +12,15 @@ public class GCD {
         String[] answers = new String[questionsCount];
         String taskDescription = "Find the greatest common divisor of given numbers.";
         for (var i = 0; i < questionsCount; i++) {
-            randomNum1 = (int) Math.ceil(Math.random() * maxRandomNumber);
-            randomNum2 = (int) Math.ceil(Math.random() * maxRandomNumber);
+            randomNum1 = (int) (Math.random() * maxRandomNumber) + 1;
+            randomNum2 = (int) (Math.random() * maxRandomNumber) + 1;
             int maxGCD = Math.min(randomNum1, randomNum2);
             int gcd = 1;
             for (var n = 1; n <= maxGCD; n++) {
-                if (randomNum1 % n == 0 && randomNum2 % n == 0) {
-                    gcd = n;
-                }
+                if (randomNum1 % n == 0)
+                    if (randomNum2 % n == 0) {
+                        gcd = n;
+                    }
             }
             questions[i] = Integer.toString(randomNum1) + " " + Integer.toString(randomNum2);
             answers[i] = Integer.toString(gcd);
