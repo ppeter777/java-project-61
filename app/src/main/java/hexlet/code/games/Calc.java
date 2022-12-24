b018583aa05e2a499a10841b4ba5f7a7e54ada87
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 02:03:03 +0300

    code simplifications in Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -16,22 +16,24 @@ public class Calc {
         String taskDescription = "What is the result of the expression?";
         for (var i = 0; i < questionsCount; i++) {
             int operationCode = (int) (Math.random() * operationsCount);
+            int result = 0;
             if (operationCode == 0) {
                 operation = " + ";
                 operand1 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
                 operand2 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
-                answers[i] = Integer.toString(operand1 + operand2);
+                result = operand1 + operand2;
             } else if (operationCode == 1) {
                 operation = " - ";
                 operand1 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
                 operand2 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
-                answers[i] = Integer.toString(operand1 - operand2);
+                result = operand1 - operand2;
             } else if (operationCode == 2) {
                 operation = " * ";
                 operand1 = (int) (Math.random() * maxRandomForMultiplication);
                 operand2 = (int) (Math.random() * maxRandomForMultiplication);
-                answers[i] = Integer.toString(operand1 * operand2);
+                result = operand1 * operand2;
             }
+            answers[i] = Integer.toString(result);
             questions[i] = Integer.toString(operand1) + operation + Integer.toString(operand2);
         }
         engine(taskDescription, questions, answers);
