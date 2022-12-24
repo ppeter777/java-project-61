9b8ad33b0e67f640c5c30c9e4bb6a2410b2e712d
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-24 17:43:12 +0300

    add Cli.java and Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greeting;
+
+public class Engine {
+    public static void engine(String taskDescription, String[] questions, String[] answers) {
+        int questionsCount = 3;
+        Scanner scanner = new Scanner(System.in);
+        String user = greeting();
+        System.out.println(taskDescription);
+        for (var i = 0; i < questionsCount; i++) {
+            System.out.println("Question: " + questions[i]);
+            System.out.print("Your answer:  ");
+            String input = scanner.next();
+            if (input.equals(answers[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("'" + input + "' is wrong answer ;(. Correct answer was '" + answers[i] + "'");
+                System.out.println("Let's try again, " + user + "!");
+                scanner.close();
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + user + "!");
+    }
+}
