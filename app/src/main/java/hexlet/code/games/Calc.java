8cb949155b2a589add4a28dd108bbb4901b91a2f
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 10:58:28 +0300

    simplify Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -10,29 +10,24 @@ public class Calc {
         final var operationsCount = 3;
         String[] questions = new String[questionsCount];
         String[] answers = new String[questionsCount];
-        int operand1 = 0;
-        int operand2 = 0;
-        String operation = "";
         String taskDescription = "What is the result of the expression?";
+        String[] operations = {" + ", " - ", " * "};
         for (var i = 0; i < questionsCount; i++) {
             int operationCode = (int) (Math.random() * operationsCount);
             int result = 0;
-            operand1 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
-            operand2 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
+            int operand1 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
+            int operand2 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
             if (operationCode == 0) {
-                operation = " + ";
                 result = operand1 + operand2;
             } else if (operationCode == 1) {
-                operation = " - ";
                 result = operand1 - operand2;
             } else if (operationCode == 2) {
-                operation = " * ";
                 operand1 = (int) (Math.random() * maxRandomForMultiplication);
                 operand2 = (int) (Math.random() * maxRandomForMultiplication);
                 result = operand1 * operand2;
             }
             answers[i] = Integer.toString(result);
-            questions[i] = Integer.toString(operand1) + operation + Integer.toString(operand2);
+            questions[i] = operand1 + operations[operationCode] + operand2;
         }
         engine(taskDescription, questions, answers);
     }
