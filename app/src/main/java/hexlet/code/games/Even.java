98299ff3120fadf7d25bc8e73c426b846c12c721
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 11:40:31 +0300

    correct calculation of random

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,13 +4,13 @@ import static hexlet.code.Engine.engine;
 public class Even {
     public static void even() {
         final var questionsCount = 3;
-        final var maxRandomNumber = 100;
+        final var maxRandom = 100;
 
         String[] questions = new String[questionsCount];
         String[] answers = new String[questionsCount];
         String taskDescription = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         for (var i = 0; i < questionsCount; i++) {
-            int randomNum = (int) Math.ceil(Math.random() * maxRandomNumber);
+            int randomNum = (int) (Math.random() * maxRandom) + 1;
             questions[i] = Integer.toString(randomNum);
             if (randomNum % 2 == 0) {
                 answers[i] = "yes";
