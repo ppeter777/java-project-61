873bae56b6e557e6fb0499b1e901631a75429bee
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 11:39:16 +0300

    simplify Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,9 +5,9 @@ import static hexlet.code.Engine.engine;
 public class Calc {
     public static void calc() {
         final var questionsCount = 3;
-        final var maxRandomForAdditionAndSubtraction = 100;
-        final var maxRandomForMultiplication = 20;
+        final var maxRandom = 50;
         final var operationsCount = 3;
+
         String[] questions = new String[questionsCount];
         String[] answers = new String[questionsCount];
         String taskDescription = "What is the result of the expression?";
@@ -15,15 +15,13 @@ public class Calc {
         for (var i = 0; i < questionsCount; i++) {
             int operationCode = (int) (Math.random() * operationsCount);
             int result = 0;
-            int operand1 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
-            int operand2 = (int) (Math.random() * maxRandomForAdditionAndSubtraction);
+            int operand1 = (int) (Math.random() * maxRandom) + 1;
+            int operand2 = (int) (Math.random() * maxRandom) + 1;
             if (operationCode == 0) {
                 result = operand1 + operand2;
             } else if (operationCode == 1) {
                 result = operand1 - operand2;
             } else if (operationCode == 2) {
-                operand1 = (int) (Math.random() * maxRandomForMultiplication);
-                operand2 = (int) (Math.random() * maxRandomForMultiplication);
                 result = operand1 * operand2;
             }
             answers[i] = Integer.toString(result);
