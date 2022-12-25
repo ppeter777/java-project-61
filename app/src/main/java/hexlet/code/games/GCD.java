2a590daeb3101a47f2fcc27ffed9bd57d4b9df52
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-25 04:03:25 +0300

    add method gcdCalc to GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -10,18 +10,22 @@ public class GCD {
         String[] answers = new String[questionsCount];
         String taskDescription = "Find the greatest common divisor of given numbers.";
         for (var i = 0; i < questionsCount; i++) {
-            int randomNum1 = (int) (Math.random() * maxRandomNumber) + 1;
-            int randomNum2 = (int) (Math.random() * maxRandomNumber) + 1;
-            int maxGCD = Math.min(randomNum1, randomNum2);
-            int gcd = 1;
-            for (var n = 1; n <= maxGCD; n++) {
-                if (randomNum1 % n == 0 && randomNum2 % n == 0) {
-                    gcd = n;
-                }
-            }
-            questions[i] = randomNum1 + " " + randomNum2;
+            int random1 = (int) (Math.random() * maxRandomNumber) + 1;
+            int random2 = (int) (Math.random() * maxRandomNumber) + 1;
+            int maxGCD = Math.min(random1, random2);
+            int gcd = gcdCalc(maxGCD, random1, random2);
+            questions[i] = random1 + " " + random2;
             answers[i] = Integer.toString(gcd);
         }
         engine(taskDescription, questions, answers);
+    }
+    public static int gcdCalc(int maxGCD, int random1, int random2) {
+        int gcd = 1;
+        for (var n = 1; n <= maxGCD; n++) {
+            if (random1 % n == 0 && random2 % n == 0) {
+                gcd = n;
+            }
+        }
+        return gcd;
     }
 }
