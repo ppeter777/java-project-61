a3b36b83343200a4142cfe50b26702480f0b357b
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-29 02:35:17 +0300

    modify Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,13 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
+    private static int rounds = 3;
     public static void engine(String taskDescription, String[] questions, String[] answers) {
         final var questionsCount = 3;
 
         Scanner scanner = new Scanner(System.in);
         String user = Cli.greeting();
         System.out.println(taskDescription);
-        for (var i = 0; i < questionsCount; i++) {
+        for (var i = 0; i < rounds; i++) {
             System.out.println("Question: " + questions[i]);
             System.out.print("Your answer:  ");
             String input = scanner.next();
@@ -23,5 +24,8 @@ public class Engine {
             }
         }
         System.out.println("Congratulations, " + user + "!");
+    }
+    public static int getRounds() {
+        return rounds;
     }
 }
