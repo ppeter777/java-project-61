335b5059af0d5d1a1ec8427d0ab3bde671f5f057
Peter Pesotskiy <ppeter777@yandex.ru>
2022-12-29 02:32:12 +0300

    a

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,18 +9,28 @@ import hexlet.code.games.Prime;
 
 public class App {
     public static void main(String[] args) {
+        final var greeting = "1";
+        final var even = "2";
+        final var calc = "3";
+        final var gcd = "4";
+        final var progression = "5";
+        final var prime = "6";
+        final var exit = "0";
+
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
         String choice = scanner.next();
         switch (choice) {
-            case "1" -> Cli.greeting();
-            case "2" -> Even.even();
-            case "3" -> Calc.calc();
-            case "4" -> GCD.gcd();
-            case "5" -> Progression.progression();
-            case "6" -> Prime.prime();
+            case greeting -> Cli.greeting();
+            case even -> Even.even();
+            case calc -> Calc.calc();
+            case gcd -> GCD.gcd();
+            case progression -> Progression.progression();
+            case prime -> Prime.prime();
+            case exit -> {
+            }
             default -> throw new IllegalStateException("Unexpected value: " + choice);
         }
     }
