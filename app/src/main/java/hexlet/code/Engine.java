6f1d7f9abad3f8ed92f0d6b4e037539a7109a8e6
Peter Pesotskiy <ppeter777@yandex.ru>
2023-01-11 00:17:21 +0300

    move logic from Cli to Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Engine {
     private static final int ROUNDS = 3;
     public static void engine(String taskDescription, String[][] test) {
+        System.out.println("\nWelcome to the Brain Games!");
+        System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
-        String user = Cli.greeting();
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
         System.out.println(taskDescription);
         for (var i = 0; i < ROUNDS; i++) {
             System.out.println("Question: " + test[0][i]);
@@ -16,12 +19,12 @@ public class Engine {
                 System.out.println("Correct!");
             } else {
                 System.out.println("'" + input + "' is wrong answer ;(. Correct answer was '" + test[1][i] + "'");
-                System.out.println("Let's try again, " + user + "!");
+                System.out.println("Let's try again, " + userName + "!");
                 scanner.close();
                 return;
             }
         }
-        System.out.println("Congratulations, " + user + "!");
+        System.out.println("Congratulations, " + userName + "!");
     }
     public static int getRounds() {
         return ROUNDS;
